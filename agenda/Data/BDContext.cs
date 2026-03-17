b798072e5f0a6c63b21e4898d65b72d7c41b5294
gabriel.asantos111 <gabriel.asantos111@JUN0684711W11-1>
2026-03-17 16:52:52 -0300

    ultima tentativa

---
+++ agenda/Data/BDContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using agenda.Models;
+
+namespace agenda.Data
+{
+    public class BDContext : DbContext
+    {
+        public BDContext (DbContextOptions<BDContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<agenda.Models.Agenda> Agenda { get; set; } = default!;
+    }
+}
---
+++ agenda/Models/Agenda.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+namespace agenda.Models
+{
+    public class Agenda
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+    }
+}
