81822ce742a90779162b05b7a2af2ad2fa8a5497
gabriel.asantos111 <gabrielaraujo20079@gmail.com>
2026-04-17 14:28:25 -0300

    itens_vendas Esta estavel agora

--- agenda/Controllers/ItemVendasController.cs
+++ agenda/Controllers/ItemVendasController.cs
@@ -28,17 +28,10 @@ namespace agenda.Controllers
         // GET: ItemVendas/Details/5
         public async Task<IActionResult> Details(int? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            if (id == null) return NotFound();
 
-            var itemVenda = await _context.Itens_Vendas
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (itemVenda == null)
-            {
-                return NotFound();
-            }
+            var itemVenda = await _context.Itens_Vendas.FirstOrDefaultAsync(m => m.Id == id);
+            if (itemVenda == null) return NotFound();
 
             return View(itemVenda);
         }
@@ -46,52 +39,67 @@ namespace agenda.Controllers
         // GET: ItemVendas/Create
         public IActionResult Create()
         {
+            ViewBag.Produtos = new SelectList(_context.Produtos, "Id", "Nome");
+            ViewBag.Clientes = new SelectList(_context.Agenda, "Id", "Nome");
             return View();
         }
 
         // POST: ItemVendas/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,Id_Agenda,Id_Produto,Quantidade,NomeCliente,NomeProduto,Data")] ItemVenda itemVenda)
         {
+            // Preenche os nomes automaticamente a partir dos IDs
+            var produto = await _context.Produtos.FindAsync(itemVenda.Id_Produto);
+            var cliente = await _context.Agenda.FindAsync(itemVenda.Id_Agenda);
+
+            if (produto != null) itemVenda.NomeProduto = produto.Nome;
+            if (cliente != null) itemVenda.NomeCliente = cliente.Nome;
+
+            ModelState.Remove("NomeProduto");
+            ModelState.Remove("NomeCliente");
+
             if (ModelState.IsValid)
             {
                 _context.Add(itemVenda);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
+
+            ViewBag.Produtos = new SelectList(_context.Produtos, "Id", "Nome");
+            ViewBag.Clientes = new SelectList(_context.Agenda, "Id", "Nome");
             return View(itemVenda);
         }
 
         // GET: ItemVendas/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            if (id == null) return NotFound();
 
             var itemVenda = await _context.Itens_Vendas.FindAsync(id);
-            if (itemVenda == null)
-            {
-                return NotFound();
-            }
+            if (itemVenda == null) return NotFound();
+
+            ViewBag.Produtos = new SelectList(_context.Produtos, "Id", "Nome", itemVenda.Id_Produto);
+            ViewBag.Clientes = new SelectList(_context.Agenda, "Id", "Nome", itemVenda.Id_Agenda);
             return View(itemVenda);
         }
 
         // POST: ItemVendas/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,Id_Agenda,Id_Produto,Quantidade,NomeCliente,NomeProduto,Data")] ItemVenda itemVenda)
         {
-            if (id != itemVenda.Id)
-            {
-                return NotFound();
-            }
+            if (id != itemVenda.Id) return NotFound();
+
+            // Preenche os nomes automaticamente a partir dos IDs
+            var produto = await _context.Produtos.FindAsync(itemVenda.Id_Produto);
+            var cliente = await _context.Agenda.FindAsync(itemVenda.Id_Agenda);
+
+            if (produto != null) itemVenda.NomeProduto = produto.Nome;
+            if (cliente != null) itemVenda.NomeCliente = cliente.Nome;
+
+            ModelState.Remove("NomeProduto");
+            ModelState.Remove("NomeCliente");
 
             if (ModelState.IsValid)
             {
@@ -102,34 +110,24 @@ namespace agenda.Controllers
                 }
                 catch (DbUpdateConcurrencyException)
                 {
-                    if (!ItemVendaExists(itemVenda.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
+                    if (!ItemVendaExists(itemVenda.Id)) return NotFound();
+                    else throw;
                 }
                 return RedirectToAction(nameof(Index));
             }
+
+            ViewBag.Produtos = new SelectList(_context.Produtos, "Id", "Nome", itemVenda.Id_Produto);
+            ViewBag.Clientes = new SelectList(_context.Agenda, "Id", "Nome", itemVenda.Id_Agenda);
             return View(itemVenda);
         }
 
         // GET: ItemVendas/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            if (id == null) return NotFound();
 
-            var itemVenda = await _context.Itens_Vendas
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (itemVenda == null)
-            {
-                return NotFound();
-            }
+            var itemVenda = await _context.Itens_Vendas.FirstOrDefaultAsync(m => m.Id == id);
+            if (itemVenda == null) return NotFound();
 
             return View(itemVenda);
         }
@@ -144,9 +142,17 @@ namespace agenda.Controllers
             {
                 _context.Itens_Vendas.Remove(itemVenda);
             }
-
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
+        }
+
+        // API: retorna nome do produto por ID (usada pelo JS)
+        [HttpGet]
+        public async Task<IActionResult> GetProduto(int id)
+        {
+            var produto = await _context.Produtos.FindAsync(id);
+            if (produto == null) return NotFound();
+            return Json(new { nome = produto.Nome });
         }
 
         private bool ItemVendaExists(int id)
